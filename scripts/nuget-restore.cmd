3acf0e122b8cb54d1ee4c23e56a39848a56158af
Jay Bazuzi <JAY@BAZUZI.COM>
2015-08-15 12:38:16 -0700

    Print actual errorlevel

--- scripts/nuget-restore.cmd
+++ scripts/nuget-restore.cmd
@@ -1,6 +1,5 @@
 @echo off
 rem initiate the retry number
-set errorCode=1
 set retryNumber=0
 set maxRetries=3
 
@@ -8,8 +7,8 @@ set maxRetries=3
 nuget restore %*
 
 rem problem?
-IF NOT ERRORLEVEL %errorCode% GOTO :EOF
-@echo Oops, nuget restore exited with code %errorCode% - let us try again!
+IF NOT ERRORLEVEL 1 GOTO :EOF
+@echo Oops, nuget restore exited with code %ERRORLEVEL% - let us try again!
 set /a retryNumber=%retryNumber%+1
 IF %reTryNumber% LSS %maxRetries% (GOTO :RESTORE)
 IF %retryNumber% EQU %maxRetries% (GOTO :ERR)
