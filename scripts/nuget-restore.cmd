e262e1b01a1d15befd52c620f8d6e4c45e4ace37
Jay Bazuzi <JAY@BAZUZI.COM>
2015-08-15 12:38:32 -0700

    Remove unnecessary IF/GOTO

--- scripts/nuget-restore.cmd
+++ scripts/nuget-restore.cmd
@@ -11,8 +11,5 @@ IF NOT ERRORLEVEL 1 GOTO :EOF
 @echo Oops, nuget restore exited with code %ERRORLEVEL% - let us try again!
 set /a retryNumber=%retryNumber%+1
 IF %reTryNumber% LSS %maxRetries% (GOTO :RESTORE)
-IF %retryNumber% EQU %maxRetries% (GOTO :ERR)
-
-:ERR
 @echo Sorry, we tried restoring nuget packages for %maxRetries% times and all attempts were unsuccessful!
 EXIT /B 1
