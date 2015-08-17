2ee9d1b5d5ecd5c52898706fd7ad8d68fa09d885
Feodor Fitsner <feodor@appveyor.com>
2015-08-17 15:49:46 -0700

    fix-iojs-node-alias added

---
+++ scripts/fix-iojs-node-alias.ps1
@@ -0,0 +1,17 @@
+﻿function FixNodeExe($iojsPath) {
+    if(-not (Test-Path $iojsPath)) {
+        return
+    }
+
+    Remove-Item "$iojsPath\node.exe" -Force
+
+    '@IF EXIST "%~dp0\iojs.exe" ( 
+       "%~dp0\iojs.exe" %* 
+    ) ELSE ( 
+       iojs %* 
+    )' | Out-File "$iojsPath\node.cmd" -Encoding ascii
+}
+
+
+FixNodeExe "${env:ProgramFiles(x86)}\iojs"
+FixNodeExe "${env:ProgramFiles}\iojs"
