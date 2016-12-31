c77614ec9558fe22ed02d82e302fa9ae5f44a43d
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-12-31 00:57:46 -0800

    Update install_xunit_192.ps1

--- scripts/enterprise/install_xunit_192.ps1
+++ scripts/enterprise/install_xunit_192.ps1
@@ -1,7 +1,7 @@
 ﻿Write-Host "Installing xUnit 1.9.2..." -ForegroundColor Cyan
 $xunitPath = "$env:SYSTEMDRIVE\Tools\xUnit"
 
-Remove-Item $xunitPath -Recurse -Force
+Remove-Item $xunitPath -Recurse -Force -ErrorAction SilentlyContinue
 
 $zipPath = "$($env:USERPROFILE)\xunit-build-1.9.2.zip"
 (New-Object Net.WebClient).DownloadFile('http://www.appveyor.com/downloads/xunit-build-1.9.2.zip', $zipPath)
