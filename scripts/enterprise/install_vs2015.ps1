e8172ca65b885c61f2e0e384543a5941a293413d
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-12-30 22:21:57 -0800

    Update install_vs2015.ps1

--- scripts/enterprise/install_vs2015.ps1
+++ scripts/enterprise/install_vs2015.ps1
@@ -1,7 +1,4 @@
-﻿# installing with Web PI
 Write-Host "Installing Visual Studio 2015 Community..." -ForegroundColor Cyan
-cmd /c start /wait webpicmd /Install /Products:"VS2015CommunityAzurePack.2.9" /AcceptEula /SuppressReboot
-Write-Host "Visual Studio 2015 Community installed" -ForegroundColor Green
-
-# add msbuild to path
+webpicmd /Install /Products:"VS2015CommunityAzurePack.2.9" /AcceptEula /SuppressReboot
 Add-Path 'C:\Program Files (x86)\MSBuild\14.0\Bin'
+Write-Host "Visual Studio 2015 Community installed" -ForegroundColor Green
