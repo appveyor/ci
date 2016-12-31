0179eb0c8d34d6c0776e7352a8b522e4e7705d38
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-12-30 23:38:49 -0800

    Update install_vs2015.ps1

--- scripts/enterprise/install_vs2015.ps1
+++ scripts/enterprise/install_vs2015.ps1
@@ -1,6 +1,13 @@
 Write-Host "Installing Visual Studio 2015 Community..." -ForegroundColor Cyan
-webpicmd /Install /Products:"VS2015_CE_Only" /AcceptEula /SuppressReboot
-Add-Path 'C:\Program Files (x86)\MSBuild\14.0\Bin'
-Add-Path 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE'
-Add-Path 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow'
+
+$vs14Path = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE"
+
+if(-not (Test-Path $vs14Path)) {
+    webpicmd /Install /Products:"VS2015_CE_Only" /AcceptEula /SuppressReboot
+}
+
+Add-Path "${env:ProgramFiles(x86)}\MSBuild\14.0\Bin"
+Add-Path $vs14Path
+Add-Path "$vs14Path\IDE\CommonExtensions\Microsoft\TestWindow"
+
 Write-Host "Visual Studio 2015 Community installed" -ForegroundColor Green
