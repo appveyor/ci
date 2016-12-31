5a9efdae82f975db091fddbec10366df1fe7f549
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-12-30 22:25:10 -0800

    Update install_vs2015.ps1

--- scripts/enterprise/install_vs2015.ps1
+++ scripts/enterprise/install_vs2015.ps1
@@ -1,4 +1,4 @@
 Write-Host "Installing Visual Studio 2015 Community..." -ForegroundColor Cyan
-webpicmd /Install /Products:"VS2015CommunityAzurePack.2.9" /AcceptEula /SuppressReboot
+webpicmd /Install /Products:"VS2015_CE_Only" /AcceptEula /SuppressReboot
 Add-Path 'C:\Program Files (x86)\MSBuild\14.0\Bin'
 Write-Host "Visual Studio 2015 Community installed" -ForegroundColor Green
