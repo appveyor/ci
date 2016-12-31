ceb41d8ac17897aa1c48c8bcc875e2bd637b38f9
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-12-30 23:19:35 -0800

    Update install_vs2013.ps1

--- scripts/enterprise/install_vs2013.ps1
+++ scripts/enterprise/install_vs2013.ps1
@@ -1,4 +1,5 @@
 Write-Host "Installing Visual Studio 2013 Community ..." -ForegroundColor Cyan
 webpicmd /Install /Products:"VS2013_CE_Only" /AcceptEula /SuppressReboot
 Add-Path "${env:ProgramFiles(x86)}\MSBuild\12.0\Bin"
+Add-Path 'C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE'
 Write-Host "Visual Studio 2013 Community installed" -ForegroundColor Green
