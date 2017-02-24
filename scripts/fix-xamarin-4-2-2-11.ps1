b9a494a1bb9aefd6f2239addbf11f038108d540f
Feodor Fitsner <feodor@appveyor.com>
2017-02-24 11:58:56 -0800

    Update fix-xamarin-4-2-2-11.ps1

--- scripts/fix-xamarin-4-2-2-11.ps1
+++ scripts/fix-xamarin-4-2-2-11.ps1
@@ -1,5 +1,5 @@
 $xmlPath = 'C:\Program Files (x86)\MSBuild\Xamarin\Android\Xamarin.Android.Common.Debugging.targets'
 $xml = [xml](get-content $xmlPath)
 $target = $xml.Project.Target | where {$_.Name -eq "_GetPrimaryCpuAbi"}
-$target.RemoveChild($target.FirstChild)
+$target.RemoveChild($target.FirstChild) | Out-Null
 $xml.Save($xmlPath)
