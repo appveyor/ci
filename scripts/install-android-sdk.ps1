e2c440b6ca7e406782a719645e30dc550324638c
Feodor Fitsner <feodor@appveyor.com>
2017-03-19 21:10:59 -0700

    Create install-android-sdk.ps1

---
+++ scripts/install-android-sdk.ps1
@@ -0,0 +1,25 @@
+Function Get-AndroidSDKs() { 
+    $output = & $AndroidToolPath list sdk --all 
+    $sdks = $output |% { 
+        if ($_ -match '(?<index>\d+)- (?<sdk>.+), revision (?<revision>[\d\.]+)') { 
+            $sdk = New-Object PSObject 
+            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Index -Value $Matches.index 
+            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Name -Value $Matches.sdk 
+            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Revision -Value $Matches.revision 
+            $sdk 
+        } 
+    } 
+    $sdks 
+}
+
+Function Install-AndroidSDK() { 
+    [CmdletBinding()] 
+    Param( 
+        [Parameter(Mandatory=$true, Position=0)] 
+        [PSObject[]]$sdks 
+    )
+
+    $sdkIndexes = $sdks |% { $_.Index } 
+    $sdkIndexArgument = [string]::Join(',', $sdkIndexes) 
+    Echo 'y' | & $AndroidToolPath update sdk -u -a -t $sdkIndexArgument 
+}
