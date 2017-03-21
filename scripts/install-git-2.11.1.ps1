17e74dece4dc3f5d9101cfee7873056a975dcee7
Feodor Fitsner <feodor@appveyor.com>
2017-03-20 23:19:32 -0700

    Update install-git-2.11.1.ps1

--- scripts/install-git-2.11.1.ps1
+++ scripts/install-git-2.11.1.ps1
@@ -1,6 +1,6 @@
 Write-Host "Uninstalling existing Git..."
 "`"C:\Program Files\Git\unins000.exe`" /silent" | out-file "$env:TEMP\uninstall-git.cmd" -Encoding ASCII
-& $env:TEMP\uninstall-git.cmd
+& $env:TEMP\uninstall-git.cmd | Out-Null
 Write-Host "Downloading Git 2.11.1..."
 $exePath = "$env:TEMP\Git-2.11.1-64-bit.exe"
 (New-Object Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.11.1.windows.1/Git-2.11.1-64-bit.exe', $exePath)
