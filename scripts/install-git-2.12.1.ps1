2fbf5cbeafd0a73bef54b16b4f6371adf8368627
Feodor Fitsner <feodor@appveyor.com>
2017-03-22 16:21:58 -0700

    Create install-git-2.12.1.ps1

---
+++ scripts/install-git-2.12.1.ps1
@@ -0,0 +1,5 @@
+Write-Host "Installing Git 2.12.1..." -NoNewLine
+$exePath = "$env:TEMP\Git-2.12.1-64-bit.exe"
+(New-Object Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.12.1.windows.1/Git-2.12.1-64-bit.exe', $exePath)
+cmd /c start /wait $exePath /VERYSILENT /NORESTART /NOCANCEL /SP- /NOICONS /COMPONENTS="icons,icons\quicklaunch,ext,ext\reg,ext\reg\shellhere,ext\reg\guihere,assoc,assoc_sh"
+Write-Host "OK" -ForegroundColor Green
