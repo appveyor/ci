f3b3ac1239f8221fea4d58db0952a4e89724f63b
Feodor Fitsner <feodor@appveyor.com>
2017-03-29 11:18:01 -0700

    Create install-git-2.11.1-v2.ps1

---
+++ scripts/install-git-2.11.1-v2.ps1
@@ -0,0 +1,5 @@
+Write-Host "Installing Git 2.11.1..." -NoNewLine
+$zipPath = "$env:TEMP\PortableGit-2.11.1-64-bit.7z.exe"
+(New-Object Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.11.1.windows.1/PortableGit-2.11.1-64-bit.7z.exe', $zipPath)
+7z x $zipPath -aoa -o'C:\Projects\2' mingw64\bin | Out-Null
+Write-Host "OK" -ForegroundColor Green
