1c2c79f50ee09c076bd7e6bc3e7901d7ca211914
Ilya Finkelshteyn <ilyaf@appveyor.com>
2017-09-08 11:02:33 -0700

    Create disable_new_network_location_wizard.ps1

---
+++ scripts/enterprise/disable_new_network_location_wizard.ps1
@@ -0,0 +1,2 @@
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff /f
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Network\NetworkLocationWizard /v HideWizard /t REG_DWORD /d 1 /f
