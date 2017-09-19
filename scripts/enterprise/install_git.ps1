61c5bc57539d3d353da6eb3afd389a759e3dafce
Feodor Fitsner <feodor@appveyor.com>
2017-09-18 17:04:17 -0700

    Update install_git.ps1

--- scripts/enterprise/install_git.ps1
+++ scripts/enterprise/install_git.ps1
@@ -1,9 +1,9 @@
-﻿Write-Host "Installing Git..." -ForegroundColor Cyan
+Write-Host "Installing Git 2.14.1..." -ForegroundColor Cyan
 
-$exePath = "$env:USERPROFILE\Git-2.8.2-64-bit.exe"
+$exePath = "$env:TEMP\Git-2.14.1-64-bit.exe"
 
 Write-Host "Downloading..."
-(New-Object Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.8.2.windows.1/Git-2.8.2-64-bit.exe', $exePath)
+(New-Object Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.14.1.windows.1/Git-2.14.1-64-bit.exe', $exePath)
 
 Write-Host "Installing..."
 cmd /c start /wait $exePath /VERYSILENT /NORESTART /NOCANCEL /SP- /NOICONS /COMPONENTS="icons,icons\quicklaunch,ext,ext\reg,ext\reg\shellhere,ext\reg\guihere,assoc,assoc_sh" /LOG
@@ -15,8 +15,9 @@ $env:path = "$env:ProgramFiles\Git\cmd;$env:path"
 Add-Path "$env:ProgramFiles\Git\usr\bin"
 $env:path = "$env:ProgramFiles\Git\usr\bin;$env:path"
 
-Remove-Item 'C:\Program Files\Git\mingw64\etc\gitconfig'
+#Remove-Item 'C:\Program Files\Git\mingw64\etc\gitconfig'
 git config --global core.autocrlf input
+git config --system --unset credential.helper
 #git config --global credential.helper store
 
 git --version
