b00b4316a42905de73fe925b79aace1a524b432d
Ilya Finkelshteyn <ilyaf@appveyor.com>
2017-10-16 17:29:03 -0700

    Create add_appveyor_host_agent_to_auto_run.ps1

---
+++ scripts/enterprise/add_appveyor_host_agent_to_auto_run.ps1
@@ -0,0 +1,7 @@
+if (Get-Service Appveyor.HostAgent -ErrorAction Ignore) {
+  Stop-Service Appveyor.HostAgent -Force
+  Set-Service Appveyor.HostAgent -StartupType Disabled
+}
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Appveyor.HostAgent" `
+	-Value "C:\Program Files\AppVeyor\HostAgent\Appveyor.HostAgent.exe"
+ 
