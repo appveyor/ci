8f4c240cc363a9f1ffb2d8c3193c17b239abb98a
Feodor Fitsner <feodor@appveyor.com>
2018-09-14 11:04:53 -0700

    Update fix-service-fabric-docker.ps1

--- scripts/fix-service-fabric-docker.ps1
+++ scripts/fix-service-fabric-docker.ps1
@@ -12,16 +12,4 @@ if ($dd) {
     Stop-Process $dd -Force
 }
 
-# Remove-Path "C:\Program Files\Docker\Docker\resources\bin"
-
-Write-Host "Downloading Docker CE daemon"
-
-$destPath = "C:\Program Files\Docker\DockerCE"
-$zipPath = "$env:TEMP\docker-ce.zip"
-(New-Object Net.WebClient).DownloadFile('https://download.docker.com/win/static/stable/x86_64/docker-17.09.0-ce.zip', $zipPath)
-7z x $zipPath -o"$destPath" | Out-Null
-del $zipPath
-
-Add-Path "$destPath\docker"
-
-Write-Host "Docker CE installed" -ForegroundColor Green
+Write-Host "Docker stopped" -ForegroundColor Green
