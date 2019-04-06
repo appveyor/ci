8e6ac28aa9ed639dba4abcaca72c00426ea212fc
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-04-05 19:15:40 -0700

    Create docker-appveyor.psm1

---
+++ scripts/docker-appveyor.psm1
@@ -0,0 +1,22 @@
+function Switch-DockerLinux
+{
+    & "$env:ProgramFiles\Docker\Docker\DockerCli.exe" -SwitchLinuxEngine
+        $dshare = (Get-SmbShare -Name X -ErrorAction SilentlyContinue)
+    if (-not $dshare) {
+	    $deUsername = 'DockerExchange'
+	    $dePsw = "ABC" + [guid]::NewGuid().ToString() + "!"
+	    $secDePsw = ConvertTo-SecureString $dePsw -AsPlainText -Force
+	    Get-LocalUser -Name $deUsername | Set-LocalUser -Password $secDePsw
+        & $env:ProgramFiles\Docker\Docker\DockerCli.exe -Start --testftw!928374kasljf039 >$null 2>&1
+	    & $env:ProgramFiles\Docker\Docker\DockerCli.exe -Mount=D -Username="$env:computername\$deUsername" -Password="$dePsw" --testftw!928374kasljf039 >$null 2>&1
+		Disable-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Direction Inbound
+    }
+}
+
+function Switch-DockerWindows
+{
+    & "$env:ProgramFiles\Docker\Docker\DockerCli.exe" -SwitchWindowsEngine
+}
+
+# export module members
+Export-ModuleMember -Function Switch-DockerLinux,Switch-DockerWindows
